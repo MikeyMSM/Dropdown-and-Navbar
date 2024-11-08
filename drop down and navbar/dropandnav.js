10e6c810d61c8b1de2331126cd4989b8b106a264
MSM <101650073+MikeyMSM@users.noreply.github.com>
2024-11-08 14:05:04 +0000

    Add files via upload

---
+++ drop down and navbar/dropandnav.js
@@ -0,0 +1,9 @@
+function loadNavbar()
+{
+    fetch("navbar.html")
+      .then(response => response.text())
+      .then(data => { 
+            document.getElementById("navbar-container").innerHTML = data;
+      })
+      .catch(error => console.error('Error loading navbar:', error));
+}
